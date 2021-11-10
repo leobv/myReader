9f23dd5d66263a9ceb39ac5341e48a471e911267
leabevi <leabevi@gmail.com>
2021-11-10 16:07:52 -0300

    review searchBooks

--- src/Components/SearchBooks.js
+++ src/Components/SearchBooks.js
@@ -47,7 +47,7 @@ export default class SearchBooks extends Component {
 			});
 		} else {
 			this.setState((currentState) => ({
-				Books: currentState.Books,
+				Books: [],
 				searchError: false,
 			}));
 		}
